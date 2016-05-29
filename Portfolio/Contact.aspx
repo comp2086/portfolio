2932a6bf2137260e9922253842cd4a3beb471eaf
Alex <alex.andriishyn@gmail.com>
2016-05-29 19:51:05 -0400

    Added messenger form

--- Portfolio/Contact.aspx
+++ Portfolio/Contact.aspx
@@ -1,5 +1,34 @@
 ﻿<%@ Page Title="Contact Me" Language="C#" MasterPageFile="~/Contact.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="Portfolio.Contact1" %>
+
 <%@ MasterType VirtualPath="~/Contact.Master" %>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-2">
+                <div class="form-horizontal">
+                    <div class="form-group">
+                        <label Class="control-label" for="txtFirstName">First name: </label>
+                        <asp:TextBox CssClass="form-control" ID="txtFirstName" required="true" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label Class="control-label" for="txtLastName">Last name: </label>
+                        <asp:TextBox CssClass="form-control" ID="txtLastName" required="true" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label Class="control-label" for="txtEmail">Email: </label>
+                        <asp:TextBox CssClass="form-control" ID="txtEmail" required="true" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="form-group">
+                        <label Class="control-label" for="txtMessage">Message: </label>
+                        <asp:TextBox CssClass="form-control" ID="txtMessage" Columns="5" Rows="3" required="true" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="form-group text-right">
+                        <asp:Button CssClass="btn btn-default" ID="btnCancel" runat="server" Text="Cancel" />
+                        <asp:Button CssClass="btn btn-info" ID="btnSubmit" runat="server" Text="Send" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- Portfolio/Contact.aspx.designer.cs
+++ Portfolio/Contact.aspx.designer.cs
@@ -13,6 +13,60 @@ namespace Portfolio {
     public partial class Contact1 {
         
         /// <summary>
+        /// txtFirstName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFirstName;
+        
+        /// <summary>
+        /// txtLastName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtLastName;
+        
+        /// <summary>
+        /// txtEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+        
+        /// <summary>
+        /// txtMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMessage;
+        
+        /// <summary>
+        /// btnCancel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCancel;
+        
+        /// <summary>
+        /// btnSubmit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSubmit;
+        
+        /// <summary>
         /// Master property.
         /// </summary>
         /// <remarks>
