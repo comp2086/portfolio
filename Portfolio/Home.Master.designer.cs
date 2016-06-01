4b2c76b87511a3dd952adca7df2822ebaeb70fc0
Alex <alex.andriishyn@gmail.com>
2016-05-31 22:21:17 -0400

    Added sticky footer

--- Portfolio/Home.Master.designer.cs
+++ Portfolio/Home.Master.designer.cs
@@ -56,5 +56,14 @@ namespace Portfolio {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        
+        /// <summary>
+        /// Footer control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Portfolio.User_Controls.Footer Footer;
     }
 }
---
+++ Portfolio/User_Controls/Footer.ascx
@@ -0,0 +1,11 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Footer.ascx.cs" Inherits="Portfolio.User_Controls.Footer" %>
+
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 col-md-offset-5">
+                <i class="fa fa-copyright"></i>Alex Andriishyn, 2016
+            </div>
+        </div>
+    </div>
+</footer>
